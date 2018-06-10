16bc5908aef83f329db5287207af2499845ceaf9
桧山正晃 <09s2035m@gmail.com>
2018-06-10 15:59:52 +0900

    Prefab作成 ランダムに力を加える機能追加

---
+++ Assets/Script/eraser.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eraser : MonoBehaviour {
+    public float ForceScale;
+    public float PointScale;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKeyDown)
+        {
+            RandomForce();
+        }		
+	}
+
+    void RandomForce()
+    {
+        Vector2 Force = new Vector2(Random.Range(-1 * ForceScale, ForceScale), Random.Range(-1 * ForceScale, ForceScale));
+        Vector2 Point = gameObject.GetComponent<Rigidbody2D>().position + new Vector2(Random.Range(-1 * PointScale, PointScale), Random.Range(-1 * PointScale, PointScale));
+
+        gameObject.GetComponent<Rigidbody2D>().AddForceAtPosition(Force, Point);
+    }
+}
